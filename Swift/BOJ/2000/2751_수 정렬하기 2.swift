920a30ce8aa3511f79b7fdff2ed99082d79b4263
knockknock <66904886+vichye-1@users.noreply.github.com>
2023-09-05 18:06:25 +0900

    Update 2751_수 정렬하기 2.swift

--- Swift/BOJ/2000/2751_수 정렬하기 2.swift
+++ Swift/BOJ/2000/2751_수 정렬하기 2.swift
@@ -2,5 +2,5 @@ var array = [Int]()
 for _ in 1...Int(readLine()!)! {
   array.append(Int(readLine()!)!)
 }
-
-print(array.sorted().map{ String($0) }.joined(separator: "\n"))
+array.sort()
+print(array.map{ String($0) }.joined(separator: "\n"))
