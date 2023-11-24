adb7a280377f7f0cefe03c112037cee476ead419
knockknock <66904886+vichye-1@users.noreply.github.com>
2023-11-24 22:25:21 +0900

    Create [3차] 압축.py

---
+++ Python/Programmers/Lv. 2/[3차] 압축.py
@@ -0,0 +1,22 @@
+def solution(msg):
+    answer = []
+    # {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8, 'I': 9, 'J': 10, 'K': 11, 'L': 12, 'M': 13, 'N': 14, 'O': 15, 'P': 16, 'Q': 17, 'R': 18, 'S': 19, 'T': 20, 'U': 21, 'V': 22, 'W': 23, 'X': 24, 'Y': 25, 'Z': 26}
+    alpha = {}
+    strings = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    # make alpha dict 
+    for (i, value) in enumerate(strings):
+        alpha[value] = i + 1
+
+    w, c = 0, 0
+    while True:
+        c += 1
+        if c == len(msg):
+            answer.append(alpha[msg[w : c]])
+            break
+        if msg[w : c + 1] not in alpha:
+            alpha[msg[w : c + 1]] = len(alpha) + 1
+            answer.append(alpha[msg[w : c]])
+            w = c
+
+    return answer
