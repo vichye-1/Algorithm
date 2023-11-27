e07452b9eaa00cd795dcabced6844aa7ce151689
knockknock <66904886+vichye-1@users.noreply.github.com>
2023-11-27 21:38:45 +0900

    파스칼의 삼각형 / 55분 / O

--- Python/BOJ/10000/16395_파스칼의 삼각형.py
+++ Python/BOJ/10000/16395_파스칼의 삼각형.py
@@ -2,3 +2,7 @@ import math
 
 row, th = map(int, input().split())
 print(math.comb(row - 1, th - 1))
+
+# math.comb(n, r) , math.perm(n, r) 을 처음 안 날. 
+# 단순히 개수만 구하고 싶으면 itertools > len(list(combinations()))를 쓰지 않아도 됨.
+# 오히려 'itertools > len(list(combinations()))'를 쓰면 메모리 초과가 남
