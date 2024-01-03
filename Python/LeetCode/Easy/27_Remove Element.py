1595ecd28b854a73d51591cc85fa8754a0187135
knockknock <66904886+vichye-1@users.noreply.github.com>
2024-01-03 21:07:13 +0900

    27_Remove Element / 5min / O

--- Python/LeetCode/Easy/27_Remove Element.py
+++ Python/LeetCode/Easy/27_Remove Element.py
@@ -1,9 +1,22 @@
+# 1
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
         count = 0
         for i in range(len(nums)):
-            i -= count
-            if nums[i] == val:
-                nums.remove(nums[i])
+            if nums[i] != val:
+                nums[count] = nums[i]
                 count += 1
-        return len(nums)
+        # nums1 = [2, 2, 2, 3]
+        # nums2 = [0, 1, 3, 0, 4, 0, 4, 2]
+        return count
+
+# 2. This answer accepted but did not meet the in-place condition.
+# class Solution:
+#     def removeElement(self, nums: List[int], val: int) -> int:
+#         count = 0
+#         for i in range(len(nums)):
+#             i -= count
+#             if nums[i] == val:
+#                 nums.remove(nums[i])
+#                 count += 1
+#         return len(nums)
