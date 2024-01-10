1dfa0c14c2361f36582dee245db5f88aca1e4fbe
knockknock <66904886+vichye-1@users.noreply.github.com>
2024-01-10 21:30:39 +0900

    125_valid Palindrome/ 5min / O

--- Python/LeetCode/Easy/125_valid Palindrome.py
+++ Python/LeetCode/Easy/125_valid Palindrome.py
@@ -10,4 +10,13 @@ class Solution:
             return True
         else:
             return False
-        
+
+# 2번째 풀이(24. 1. 10. 수)
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        result = ""
+        for c in s:
+            if c.isalnum():
+                result += c
+        result = result.lower()
+        return result == result[::-1]
